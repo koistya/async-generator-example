85c669d6697ef6edfb007cbf0ebdd2a4fb738530
Konstantin Tarkus <koistya@gmail.com>
2019-07-11 02:43:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const data = [1,2,3,4,5,6,7];
+const Promise = require('bluebird'); // v3.5.5
+const data = [1, 2, 3, 4, 5, 6, 7];
 
 async function* next() {
   let offset = 0;
@@ -6,6 +7,7 @@ async function* next() {
 
   do {
     batch = await data.slice(offset, offset + 3);
+
     for (const val of batch) {
       offset++;
       yield val;
@@ -14,11 +16,21 @@ async function* next() {
 }
 
 (async () => {
-  // await Promise.map(next(), val => {
-  //   console.log(val);
-  // });
+
+  console.log('---');
+  console.log('for await (const val of next()) { ... }\n');
 
   for await (const val of next()) {
     console.log(val);
   }
+
+  console.log('---');
+  console.log('Promise.map(Array.from({ length }), () => { ... })\n');
+  
+  const iterator = next();
+  await Promise.map(Array.from({ length: data.length }), async () => {
+    const { value } = await iterator.next();
+    console.log(value);
+  }, { concurrency: 2 });
+
 })().catch(console.error);
